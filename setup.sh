5e1ef5634cccd6020fb606688ac09582804b6a97
James <james@IBM-PW02WX8B.>
2025-01-24 23:13:06 -0500

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env bash
 
 # Exit on error
-set -e
+set -eu
 
 # Detect OS
-current_os = ""
+current_os=""
 if [ -n "$WSL_DISTRO_NAME" ]; then
-	current_os = "wsl"
-elif [[ $(uname) = "Darwin" ]]; then
-	current_os = "mac"
+	current_os="wsl"
+elif [[ "$(uname)" = "Darwin" ]]; then
+	current_os="mac"
 elif [ "$(uname)" = "Linux" ]; then
 	current_os="linux"
 else
 	echo "current os not wsl/mac/linux existing..."
 	exit 0
 fi
-echo "current os is $(current_os)"
+echo "current os is $current_os"
 
 #install devbox
-export 
 if command -v devbox &> /dev/null; then
     echo "devbox is installed"
     devbox version
@@ -32,7 +31,7 @@ fi
 echo "installing devbox packages..."
 devbox global add _1password git go-task jq lsd neovim oh-my-posh python@3.11 stow zellij 
 devbox global add xclip zsh --exclude-platform aarch64-darwin,aarch-64-linux
-if [ "$current_os" != "wsl" ];
+if [ "$current_os" != "wsl" ]; then
 	devbox global add ghostty
 fi
 devbox global install
@@ -43,41 +42,42 @@ echo "setting up symlinks with stow..."
 stow --adopt */
 git restore .
 
+#posh setup
+oh-my-posh font install meslo
+
 #source .inboundrc from .zshrc
-echo "adding ~/.ibmshellrc to zsh and bash rc files..."
+echo "adding ~/.shellrc to zsh and bash rc files..."
 touch ~/.zshrc
-if ! grep -q "\\[ -f ~/.ibmshellrc \\] && source ~/.ibmshellrc" ~/.zshrc; then
+if ! grep -q "\[ -f ~/.shellrc \] && source ~/.shellrc" ~/.zshrc; then
     echo "Adding source line to ~/.zshrc"
-    echo '[ -f ~/.ibmshellrc ] && source ~/.ibmshellrc' >> ~/.zshrc
+    echo '[ -f ~/.shellrc ] && source ~/.shellrc' >> ~/.zshrc
 fi
 
 #source .inboundrc from .bashrc
 touch ~/.bashrc
-if ! grep -q "\\[ -f ~/.ibmshellrc \\] && source ~/.ibmshellrc" ~/.bashrc; then
+if ! grep -q "\[ -f ~/.shellrc \] && source ~/.shellrc" ~/.bashrc; then
    echo "Adding source line to ~/.bashrc"
-   echo '[ -f ~/.ibmshellrc ] && source ~/.ibmshellrc' >> ~/.bashrc
+   echo '[ -f ~/.shellrc ] && source ~/.shellrc' >> ~/.bashrc
 fi
 
 #restart shell
-echo 'restarting shell...'
+echo 'sourcing shell config...'
 if [ -n "$ZSH_VERSION" ]; then
-    exec zsh
+	source ~/.zshrc
 elif [ -n "$BASH_VERSION" ]; then
-    exec bash
+	source ~/.bashrc
 else
     echo "Unknown shell. may not work outside zsh and bash. restart to try."
 fi
 
-#set git in wsl to use windows ssh for 1password
+#set git ssh to 1password in wsl
+echo 'setting ssh to 1password in wsl'
 if [ "$current_os" = "wsl" ]; then
 	git config --global core.sshCommand ssh.exe
 fi
 
-#posh setup
-oh-my-posh font install meslo
-
 #zsh setup
-if [ "$current_os" != "osx" ]; then
+if [ "$current_os" != "mac" ]; then
 	#add devbox to shell list
 	DEVBOX_ZSH=$(which zsh)
 	if ! grep -q "$DEVBOX_ZSH" /etc/shells; then
@@ -85,20 +85,20 @@ if [ "$current_os" != "osx" ]; then
 	fi
 	# Set devbox's zsh as default shell
 	chsh -s "$DEVBOX_ZSH"
-	# launch zsh
-	exec "$DEVBOX_ZSH"
 fi
 
 #setup 1password cli
-if [ -n "$WSL_DISTRO_NAME" ]; then
-    echo "WSL detected..."
-    if [ $(op account list 2>/dev/null | wc -l) -eq 0 ]; then
+if [ "$current_os" = "wsl" ]; then
+    echo "setting up 1password in wsl..."
+    if ! op account list | grep -q "my.1password.com" 2>/dev/null; then
         echo "setting up 1password account"
-	op account add --address my.1password.com --signin
+        if ! op account add --address my.1password.com --signin; then
+            echo "Failed to add 1Password account"
+            exit 1
+        fi
     else
         echo "1Password account already set up"
     fi
 else
-    op vault list 	
+    op vault list || echo "Error: Could not list 1Password vaults"
 fi
-
