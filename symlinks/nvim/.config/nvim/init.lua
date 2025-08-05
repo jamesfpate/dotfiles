15887793670318fd0de6561124466c3fcdab6d4f
James Pate <james@jamespate.net>
2025-08-05 13:34:50 -0400

    add grep shortcut

--- symlinks/nvim/.config/nvim/init.lua
+++ symlinks/nvim/.config/nvim/init.lua
@@ -402,6 +402,7 @@ require("lazy").setup({
         vim.keymap.set('n', '<leader>t', builtin.find_files, { desc = 'Telescope find files' })
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
         vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = 'Telescope grep string' })
