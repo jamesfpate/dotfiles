8b835661b5a43c6ece6d2ba6d45f251eb3507557
James Pate <james@jamespate.net>
2026-03-05 13:29:04 -0500

    add plugins, remove hints

--- symlinks/nvim/.config/nvim/init.lua
+++ symlinks/nvim/.config/nvim/init.lua
@@ -92,30 +92,30 @@ require("lazy").setup({
       end
     },
     {
-      "tris203/precognition.nvim",
+      "folke/which-key.nvim",
+      event = "VeryLazy",
       opts = {
-        startVisible = true,
-        showBlankVirtLine = true,
-        highlightColor = { link = "Comment" },
-        hints = {
-          Caret = { text = "^", prio = 2 },
-          Dollar = { text = "$", prio = 1 },
-          MatchingPair = { text = "%", prio = 5 },
-          Zero = { text = "0", prio = 1 },
-          w = { text = "w", prio = 10 },
-          b = { text = "b", prio = 9 },
-          e = { text = "e", prio = 8 },
-          W = { text = "W", prio = 7 },
-          B = { text = "B", prio = 6 },
-          E = { text = "E", prio = 5 },
-        },
-        gutterHints = {
-          G = { text = "G", prio = 10 },
-          gg = { text = "gg", prio = 9 },
-          PrevParagraph = { text = "{", prio = 8 },
-          NextParagraph = { text = "}", prio = 8 },
+        preset = "modern",
+      },
+      keys = {
+        {
+          "<leader>?",
+          function()
+            require("which-key").show({ global = false })
+          end,
+          desc = "Buffer Local Keymaps (which-key)",
         },
       },
+    },
+    {
+      "windwp/nvim-autopairs",
+      event = "InsertEnter",
+      config = true,
+    },
+    {
+      "lukas-reineke/indent-blankline.nvim",
+      main = "ibl",
+      opts = {},
     },
     {
       "echasnovski/mini.files",
